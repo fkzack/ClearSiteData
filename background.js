a0ae7567456b8f7d74be001bc273c2da2df946b3
fkzack <github@fzack.com>
2019-12-21 10:45:47 -0800

    clear both base url and subdomain

--- background.js
+++ background.js
@@ -1,24 +1,31 @@
 var clear_callback = function(){
    console.log('cleared');
+    
 };
 
-chrome.browserAction.onClicked.addListener(function(tab){	
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {			
+
+chrome.browserAction.onClicked.addListener(function(tab){
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
             var full_url = tabs[0].url.split('/');
             var protocol = full_url[0];
+            var subdomain = full_url[2]
             var full_host = full_url[2].split('.');
             var host
             if (full_host.length < 3) {
-            host = full_url[2];		
+            host = full_url[2];
             } else {
             host = full_host.slice(full_host.length-2, full_host.length).join(".")
             }
-
-            var base_url = protocol + '//' + host;		
-            console.log('url: ' + full_url.join("") + ' --> ' +  base_url);			
+            
+            var base_url = protocol + '//' + host;
+            var sub_url =  protocol + '//' + subdomain;
+            var hosts= [base_url, sub_url];
+            
+            console.log('clearing hosts: ' + hosts);
+            
             chrome.browsingData.remove( {
-                     "origins": [base_url]
+                     "origins": hosts
                   }, {
                      "appcache": true,
                      "cache": true,
@@ -36,7 +43,8 @@ chrome.browserAction.onClicked.addListener(function(tab){
                      "serviceWorkers": true,
                      "webSQL": true
                   }, clear_callback );
-      });	
+            
+      });
 });
 
 
